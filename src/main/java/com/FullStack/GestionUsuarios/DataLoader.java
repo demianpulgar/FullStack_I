d82176b48c2b1db1896fac4bf672cff9f0ff1a2d
demianpulgar <de.pulgar@duocuc.cl>
2025-06-10 23:03:01 -0400

    Application actualizado y dockerizado

---
+++ src/main/java/com/FullStack/GestionUsuarios/DataLoader.java
@@ -0,0 +1,40 @@
+package com.FullStack.GestionUsuarios;
+
+import com.FullStack.GestionUsuarios.Model.User;
+import com.FullStack.GestionUsuarios.Repository.UserRepository;
+import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Profile("dev")
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) {
+        Faker faker = new Faker(new Locale("es"));
+
+        for (int i = 0; i < 20; i++) { // Inserta 20 usuarios ficticios
+            User usuario = new User();
+            usuario.setName(faker.name().fullName());
+            usuario.setEmail(faker.internet().emailAddress());
+            usuario.setTelefono(faker.phoneNumber().cellPhone());
+            usuario.setRol(faker.options().option("ADMIN", "USER", "READER"));
+            usuario.setCiudad(faker.address().city());
+            usuario.setActivo(true); // o faker.bool().bool()
+            usuario.setUserPassword(faker.internet().password(8, 16));
+
+            userRepository.save(usuario);
+        }
+
+        System.out.println("✔ Usuarios ficticios cargados correctamente.");
+    }
+}
+
